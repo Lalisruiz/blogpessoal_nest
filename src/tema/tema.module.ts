ac427bad71560192d44db1d304c643a1d610d416
Larissa <larissaemini@gmail.com>
2025-08-04 17:47:18 -0300

    Feat: atualiza estrutura de pastas

--- src/tema/tema.module.ts
+++ src/tema/tema.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Tema } from "./entities/tema.Entity";
+import { Tema } from "./entities/tema.entity";
 import { TemaController } from "./controllers/tema.controller";
 import { TemaService } from "./services/tema.service";
-import { Postagem } from "../entities/postagem.entity";
+import { Postagem } from "../postagem/entities/postagem.entity";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Tema])],
